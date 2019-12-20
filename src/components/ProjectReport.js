3c8188c8313509d9bb496798679fb50ef0280618
Kelina Shrestha <kylesth@gmail.com>
2019-12-20 17:31:24 +0545

    Create form component for opentok query.

---
+++ src/components/ProjectReport.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProjectReport extends React.Component {
+  render() {
+    return (
+      <div className="container h-100">
+        <h1>Project Report Dashboard</h1>
+      </div>
+    );
+  }
+}
+
+export default ProjectReport;
---
+++ src/components/SessionReport.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SessionReport extends React.Component {
+  render() {
+    return (
+      <div className="container h-100">
+        <h1>Session Report Dashboard</h1>
+      </div>
+    );
+  }
+}
+
+export default SessionReport;
