d420bfb2566918268af7f5ee3b04cadfee010f2a
Kelina Shrestha <kylesth@gmail.com>
2020-01-20 13:04:45 +0545

    Show country stats on geochart.

--- src/components/sessions/CountryStats.js
+++ src/components/sessions/CountryStats.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
-import { Pie } from 'react-chartjs-3';
+import { Chart } from "react-google-charts";
 import { get } from 'lodash';
 import Loading from './../../components/Loading';
 import ErrorMessage from './../../components/ErrorMessage';
@@ -46,10 +46,7 @@ class CountryStats extends Component {
       var propName = obj.country;
       chartData[propName] = chartData[propName] ? chartData[propName] + 1 : 1;
     });
-    return {
-      labels: Object.keys(chartData),
-      chartData: Object.values(chartData)
-    }
+    return Object.entries(chartData)
   }
 
   render() {
@@ -61,37 +58,18 @@ class CountryStats extends Component {
           const resources = get(data, 'project.sessionData.sessions.resources', []);
           const meetingResources = resources.map(item => get(item, 'meetings.resources', []))[0];
           const connectionResources = meetingResources.map(item => get(item, 'connections.resources', []))[0];
-          const {labels, chartData} = this.getSubscribedData(connectionResources);
-
+          const labels = ['Country', 'No. of Connections'];
+          var countryData = [labels];
+          const chartData = this.getSubscribedData(connectionResources);
+          countryData = countryData.concat(chartData);
           return (
-            <Pie data={{
-              labels: labels,
-              datasets: [{
-                data: chartData,
-                backgroundColor: [
-                  '#e6194b',
-                  '#3cb44b',
-                  '#ffe119',
-                  '#4363d8',
-                  '#f58231',
-                  '#911eb4',
-                  '#46f0f0',
-                  '#f032e6',
-                  '#bcf60c',
-                  '#fabebe',
-                  '#008080',
-                  '#e6beff',
-                  '#9a6324',
-                  '#fffac8',
-                  '#800000',
-                  '#aaffc3',
-                  '#808000',
-                  '#ffd8b1',
-                  '#000075',
-                  '#808080'
-                ],
-              }],
-            }} />
+            <Chart
+            chartType="GeoChart"
+            data={countryData}
+            width="100%"
+            height="400px"
+            legendToggle
+          />
           );
         }}
       </Query>
