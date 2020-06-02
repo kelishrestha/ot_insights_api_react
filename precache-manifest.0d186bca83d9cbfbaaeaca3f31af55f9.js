b65bf9fa85986da0c27863d0083733c1fae5bd4e
Kelina Shrestha <kylesth@gmail.com>
2020-06-03 00:45:06 +0545

    Updates

---
+++ precache-manifest.0d186bca83d9cbfbaaeaca3f31af55f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4080fca6e7fc2d84afcfc5aebe4f2f33",
+    "url": "/ot_insights_api_react/index.html"
+  },
+  {
+    "revision": "0e3f5deee880483cb045",
+    "url": "/ot_insights_api_react/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "607e303fd809e2882eed",
+    "url": "/ot_insights_api_react/static/css/main.520a39f6.chunk.css"
+  },
+  {
+    "revision": "0e3f5deee880483cb045",
+    "url": "/ot_insights_api_react/static/js/2.f34a2d97.chunk.js"
+  },
+  {
+    "revision": "48938053a762a08cf7f5f5a4c1631bb4",
+    "url": "/ot_insights_api_react/static/js/2.f34a2d97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "607e303fd809e2882eed",
+    "url": "/ot_insights_api_react/static/js/main.8590deff.chunk.js"
+  },
+  {
+    "revision": "2569e5c492cd328313b0",
+    "url": "/ot_insights_api_react/static/js/runtime-main.f6030cd9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ot_insights_api_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/components/charts/helpers/round.js
+++
@@ -1,4 +0,0 @@
-const round = (value, decimals) =>
-  Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-
-export default round;
